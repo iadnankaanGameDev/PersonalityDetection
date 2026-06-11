4c82455bca005eab8ee3825cc2fd7de76159b760
iadnankaanGameDev <iadnankaan2@gmail.com>
2026-06-11 20:54:34 +0300

    Add root redirect for personality app

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import pandas as pd
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/en")
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
